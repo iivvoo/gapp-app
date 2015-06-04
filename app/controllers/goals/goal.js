824842a88bb9be40469edee480e455859c6c43b2
Ivo van der Wijk <github@in.m3r.nl>
2015-06-04 15:16:16 +0200

    nest goal controller

---
+++ app/controllers/goals/goal.js
@@ -0,0 +1,28 @@
+import Ember from "ember";
+
+export default Ember.Controller.extend({
+    needs: "goals",
+    newTask: '',
+    isEditing: false,
+
+    disabled: Ember.computed('newTask', function() {
+        return Ember.isEmpty(this.get('newTask'));
+    }),
+
+    actions: {
+        saveGoal: function() {
+            this.set('isEditing', false);
+            this.model.save();
+        },
+        cancelEdit: function() {
+            this.set('isEditing', false);
+            this.model.rollback();
+        },
+        deleteGoal() {
+            // delete related tasks or make them orphans?
+            this.model.destroyRecord().then(() => {
+                this.transitionTo('goals');
+            });
+        },
+    }
+});
