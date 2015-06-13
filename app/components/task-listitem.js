a597c92aba177a66bac60d85e0d6248d2972578a
Ivo van der Wijk <github@in.m3r.nl>
2015-06-13 10:48:28 +0200

    display deadline in task-listitem, if any

--- app/components/task-listitem.js
+++ app/components/task-listitem.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    date: function() {
+        if(this.get('task.date')) {
+            return this.get('task.date').format("YYYY-MM-DD hh:mm");
+        }
+        return null;
+    }.property("task.date"),
+
     handleCompleted: function() {
         this.task.save();
     }.observes('task.isCompleted')
