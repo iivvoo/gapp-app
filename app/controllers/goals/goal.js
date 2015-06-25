398ccd7534d4f353d764977ba85b2c031198dc04
Ivo van der Wijk <github@in.m3r.nl>
2015-06-25 17:22:57 +0200

    toggle show/hide completed

--- app/controllers/goals/goal.js
+++ app/controllers/goals/goal.js
@@ -2,13 +2,26 @@ import Ember from "ember";
 
 export default Ember.Controller.extend({
     goal: null,
-    needs: "goals",
     newTask: '',
-    isEditing: false,
-    somevar: "Hello Goal",
+
+    showCompleted: false,
+
+    queryParams: ['showCompleted'],
 
     sortProperties: ['sortableDate:asc', 'sortablePriority:desc'],
-    sortedTasks: Ember.computed.sort('goal.tasks', 'sortProperties'),
+    sortedTasks: Ember.computed.sort('tasks', 'sortProperties'),
+
+    tasks: function() {
+        return this.get('goal.tasks');
+    }.property('goal.tasks'),
+
+    tasks: function() {
+        let showCompleted = this.get('showCompleted');
+
+        return this.get('goal.tasks').filter(function(task) {
+            return showCompleted || !task.get('isCompleted');
+        });
+    }.property('goal.tasks.@each.isCompleted', 'showCompleted'),
 
     disabled: Ember.computed('newTask', function() {
         return Ember.isEmpty(this.get('newTask'));
