2984d601b0245674d1dd0560a907f43d1a29152b
Ivo van der Wijk <github@in.m3r.nl>
2015-06-27 10:57:31 +0200

    Working goal edit (on copied data)

--- app/controllers/goals/goal/edit.js
+++ app/controllers/goals/goal/edit.js
@@ -1,17 +1,30 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
+    work: function() {
+        return Ember.Object.create({
+            title: this.get('model.title'),
+            body: this.get('model.body')
+        });
+    }.property('model'),
+
     actions: {
         saveGoal: function() {
-            this.set('isEditing', false);
+            for(var attr of ["title", "body"]) {
+                this.set(`model.${attr}`, this.get(`work.${attr}`));
+            }
+
             this.model.save();
+            this.transitionToRoute('goals.goal');
         },
         cancelEdit: function() {
-            this.set('isEditing', false);
-            this.model.rollback();
+            // XXX are you sure?
+            this.transitionToRoute('goals.goal');
         },
         deleteGoal() {
             // delete related tasks or make them orphans?
+            console.log("Delete alles!");
+            return;
             this.model.destroyRecord().then(() => {
                 this.transitionTo('goals');
             });
