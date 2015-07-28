53f648574a91191bc8a14b854be4c2ce93189924
Ivo van der Wijk <github@in.m3r.nl>
2015-07-28 17:22:07 +0200

    switch to super secret online couch, for now

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -4,7 +4,8 @@ import Ember from 'ember';
 import EmberPouch from 'ember-pouch';
 
 var db = new PouchDB('local1'); //config.local_couch || 'bloggr');
-var remote = new PouchDB("http://ivo:ivo@localhost:5984/hamster", {ajax: {timeout: 20000}});
+//var remote = new PouchDB("http://ivo:ivo@localhost:5984/hamster", {ajax: {timeout: 20000}});
+var remote = new PouchDB("http://ivo:g3h31m@rommel.m3r.nl:5984/gapp-ivo", {ajax: {timeout: 20000}});
 
 db.sync(remote, {live: true, retry: true});
 
