4850643617ef8f2cbb45c44a489a2e6ca7d01888
Ivo van der Wijk <github@in.m3r.nl>
2015-09-24 18:01:53 +0200

    work on today -> put on plate

--- app/controllers/goals/goal/tasks/task/base.js
+++ app/controllers/goals/goal/tasks/task/base.js
@@ -8,11 +8,6 @@ export default Ember.Controller.extend({
     startGlobalTime:function(){
         this.set('now', Date.now());
         Ember.run.later(this, this.startGlobalTime, 2000);
-    }.on('init'),
+    }.on('init')
 
-    workon_today: function() {
-        let workon = this.get('model.workon');
-
-        return workon && moment(this.get('now')).isSame(moment(workon), "day");
-    }.property('model.workon', 'now'),
 });
