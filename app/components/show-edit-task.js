aa82deda3c3f5e26a6803c36b39e2010db8808b9
Ivo van der Wijk <github@in.m3r.nl>
2015-11-10 12:28:26 +0100

    Add delete to task modal

--- app/components/show-edit-task.js
+++ app/components/show-edit-task.js
@@ -1,15 +1,26 @@
 import Ember from 'ember';
 
+let edit =  Symbol("edit");
+let view =  Symbol("view");
+let del =  Symbol("delete");
+
 export default Ember.Component.extend({
     modal: Ember.inject.service(),
-    edit: false,
+    mode: view,
+
+    modes: {del:del, view:view, edit:edit},
+
 
     title: function() {
-        if(this.get('edit')) {
-            return 'Edit task ' + this.get('model.title');
+        switch(this.get('mode')) {
+            case edit:
+                return 'Edit task ' + this.get('model.title');
+            case view:
+                return 'Task ' + this.get('model.title');
+            case del:
+                return 'Delete task ' + this.get('model.title');
         }
-        return 'Task ' + this.get('model.title');
-    }.property('model.title', 'edit'),
+    }.property('model.title', 'mode'),
 
     date_or_notset: function() {
         return this.get('model.date') || "not set";
@@ -17,8 +28,7 @@ export default Ember.Component.extend({
 
     actions: {
         edit() {
-            console.log("edit");
-            this.set('edit', !this.get('edit'));
+            this.set('mode', edit);
         },
         save() {
             console.log("save");
@@ -27,10 +37,27 @@ export default Ember.Component.extend({
             let editform = this.get('editform');
 
             editform.saveTask().then((task) => {
-                console.log("Saved");
                 this.set('model', task);
-                this.set('edit', !this.get('edit'));
+                this.set('mode', view);
             });
+        },
+        delete() {
+            this.set('mode', del);
+        },
+        delete_confirmed() {
+            let model = this.get('model');
+
+            model.get('goal').then((goal) => {
+
+                goal.get('tasks').removeObject(this.get('model'));
+
+                goal.save().then(() => {
+                    this.get('model').destroyRecord();
+                }).then(() => {
+                    this.get('modal').close('delete');
+                    // notification? XXX
+                });
+            })
         },
         closeModal() {
             this.get('modal').close('close');
