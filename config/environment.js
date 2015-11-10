0a3ae88d632cb0852a9b0763030620cd9dc06a0b
Ivo van der Wijk <github@in.m3r.nl>
2015-11-10 18:55:38 +0100

    appcache support added

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,16 @@
 /* jshint node: true */
 
+var contentSecurityPolicy = {
+    'default-src': "'self'",
+    'frame-src': "*",
+    'script-src': "'self' 'unsafe-inline'",
+    'font-src': "'self'",
+    'connect-src': "'self' localhost:* *", // needed for couch sync
+    'img-src': "'self'",
+    'style-src': "'self' 'unsafe-inline'",
+    'media-src': "'self'"
+};
+
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'gap-app',
@@ -19,7 +30,8 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+
   };
 
   if (environment === 'development') {
@@ -28,6 +40,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.contentSecurityPolicy = contentSecurityPolicy;
   }
 
   if (environment === 'test') {
@@ -43,7 +56,14 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.manifest = {
+        enabled: true,
+        appcacheFile: "/manifest.appcache",
+        excludePaths: [],
+        includePaths: ['/'],
+        network: ['*'],
+        showCreateDate: true
+    };
   }
 
   return ENV;
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ HASH=`git show --oneline | head -1 | awk '{print $1}'`
 DATE=`date +'%Y%m%d%H%M%S'`
 DEST="$HASH-$DATE"
 
-ember build
+ember build --environment production
 rsync -av dist/ $DEPLOYHOST:gapp/$DEST
 ssh $DEPLOYHOST "cd gapp && rm -f active && ln -sf $DEST active"
 
